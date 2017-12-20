117862286713186bd12a124a2159eb83747e8413
Haseeb ur Rahman <me@haseeb.co>
2017-12-20 11:42:51 -0500

    new intents and state handlers

--- lambda/index.js
+++ lambda/index.js
@@ -1,31 +1,20 @@
 /* eslint-disable  func-names */
 /* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
 
 'use strict';
 
 const Alexa = require('alexa-sdk');
-const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+const APP_ID = 'amzn1.ask.skill.e167b868-fdc9-4289-bf7b-e372b8728b85';
+var xml2js = require('xml2js-es6-promise');
+var rp = require('request-promise');
 
-var https = require('https');
-var http = require('http');
-var xml2js = require('xml2js').parseString;
 
 const handlers = {
     'LaunchRequest': function () {
         this.emit(':ask', "What book would you like to listen to ?");
     },
-    'GetNewFactIntent': function () {
-        this.emit('GetFact');
-    },
-    'SearchBook': function(event, handler) {
 
+    'SearchBook': function(event, handler) {
 
       var slots = this.event.request.intent.slots;
 
@@ -38,39 +27,28 @@ const handlers = {
           format: 'json'
         };
 
-        var output = "Finding audiobooks for the book" + slots.bookName.value;
-        // this.emit(':tell', output);
-        this.response.audioPlayerPlay('REPLACE_ALL',
-                                      'http://ia802602.us.archive.org/9/items/pride_and_prejudice_librivox/prideandprejudice_54_austen_64kb.mp3',
-                                      'http://ia802602.us.archive.org/9/items/pride_and_prejudice_librivox/prideandprejudice_54_austen_64kb.mp3',
-                                      null, 0);
-        this.emit(':responseReady');
-        getFirstBook(options, (url_rss) => {
-          this.emit(':tell', url_rss);
-          getUrls(url_rss, (data) => {
-            this.emit(':tell', 'extracting urls');
-            console.log(data);
-            this.response
-              .audioPlayerPlay('REPLACE_ALL', data[0].url, data[0].url, null, 0);
-            this.emit(':responseReady');
-          });
+        var output = "Finding audiobooks for the book " + slots.bookName.value;
+
+        var url = buildUrl(options);
+        httpGet(url).then((data) => {
+          var results = buildSearchResults(data);
+          this.attributes['searchResults'] = results;
+          this.attributes['confirmIndex'] = 0;
+          this.response.speak('Did you mean ' + results[0].title + ' by ' +
+                              results[0].authors.join(', '));
+          this.emit(':responseReady');
+          this.handler.state = states.CONFIRMBOOK;
         });
-
-
+        // this.response.audioPlayerPlay('REPLACE_ALL',
+        //                               'https://ia802602.us.archive.org/9/items/pride_and_prejudice_librivox/prideandprejudice_54_austen_64kb.mp3',
+        //                               'https://ia802602.us.archive.org/9/items/pride_and_prejudice_librivox/prideandprejudice_54_austen_64kb.mp3',
+        //                               null, 0);
       } else {
         this.emit(':tell', "Charles Dickens");
       }
 
     },
-    'GetFact': function () {
-        const factArr = this.t('FACTS');
-        const factIndex = Math.floor(Math.random() * factArr.length);
-        const randomFact = factArr[factIndex];
 
-        // Create speech output
-        const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
-        this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
-    },
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
         const reprompt = this.t('HELP_MESSAGE');
@@ -84,38 +62,6 @@ const handlers = {
     },
 };
 
-function httpsGet(options, callback, url) {
-
-  if(url === undefined) {
-    var baseUrl = 'https://librivox.org/api/feed/' + options.feedname;
-    var reqUrl = '/?' + options.parameter +'=^' +
-          encodeURIComponent(options.query) +
-          '&format=' + options.format;
-    var url = baseUrl + reqUrl;
-  }
-
-  var method = url.startsWith('https') ? https : http;
-  var req = method.request(url, res => {
-    res.setEncoding('utf8');
-    var returnData = "";
-
-    res.on('data', chunk => {
-      returnData = returnData + chunk;
-    });
-
-    res.on('end', () => {
-      if(url.includes('rss')) {
-        xml2js(returnData, (error, result) => {
-          callback(result);
-        });
-      } else {
-        returnData = JSON.parse(returnData);
-        callback(returnData);
-      }
-    });
-  });
-  req.end();
-}
 
 var options = {
   feedname: "audiobooks",
@@ -124,45 +70,127 @@ var options = {
   format: "json"
 }
 
-function getFirstBook(options, callback) {
-  httpsGet(options, (returnData) => {
-    callback(returnData.books[0].url_rss);
+var xmlTransform = (body, response, resolveWithFullResponse) => {
+  return xml2js(body).then((result) => {
+    return result;
   });
-}
+};
 
-function getUrls(url, callback) {
-  var urls = [];
-  httpsGet({}, (data) => {
-    data.rss.channel[0].item.forEach((item) => {
-      urls.push({
-        'title': item.title[0],
-        'url': item.link[0]
-      });
+function httpGet(url) {
+  var options = {
+    uri : url
+  };
+
+  if(url.includes('rss')) {
+    options['transform'] = xmlTransform;
+  }
+
+  if(url.includes('json')) {
+    options['json'] = true;
+  }
+
+  return rp(options)
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
     });
-    callback(urls);
-  }, url);
 }
 
-// httpsGet(options, (data) => {
-//   console.log(data.rss.channel[0].item[0].link[0]);
-//   console.log(data.rss.channel[0].item[0].title[0]);
-// }, 'https://librivox.org/rss/253');
+function buildSearchResults(jsonObject) {
+  var result = [];
+  jsonObject.books.forEach((book) => {
+    var data = {};
+    data['title'] = book.title;
+    data['description'] = book.description;
+    data['url'] = book.url_rss;
+    data['authors'] = [];
+    book.authors.forEach((author) => {
+      data['authors'].push(author.first_name + ' ' + author.last_name);
+    });
+    result.push(data);
+  });
 
-// httpsGet(options, (data) => {
-//   console.log(data.books[0]);
+  // console.log(result);
+  return result;
+}
+
+function buildUrl(options) {
+  var baseUrl = 'https://librivox.org/api/feed/' + options.feedname;
+  var reqUrl = '/?' + options.parameter +'=^' +
+        encodeURIComponent(options.query) +
+        '&format=' + options.format;
+  var url = baseUrl + reqUrl;
+  return url;
+
+}
+
+const states = {
+  CONFIRMBOOK : '_CONFIRMBOOK'
+};
+
+const playHandlers = Alexa.CreateStateHandler(states.CONFIRMBOOK, {
+  'NewSession': function() {
+    this.emit('NewSession');
+  },
+
+  'AMAZON.YesIntent': function() {
+    var index = this.attributes['confirmIndex'];
+    var book = this.attributes['searchResults'][index];
+    console.log(book);
+    httpGet(book.url).then((data) => {
+      var bookData = buildBookData(data);
+      this.attributes['currentBook'] = bookData;
+      console.log('inside httpget');
+      var firstTitle = bookData.chapters[0].title;
+      var firstURL= bookData.chapters[0].url.replace(/http/, 'https');
+      console.log(firstTitle);
+      this.response.speak('Playing ' + firstTitle);
+      this.response.audioPlayerPlay('REPLACE_ALL', firstURL, firstTitle, null, 0);
+      this.emit(':responseReady');
+      });
+
+  }
+});
+function buildBookData(jsonObject) {
+  var result = {};
+  var chapters = [];
+  result['title'] = jsonObject.rss.channel[0].title;
+  result['url'] = jsonObject.rss.channel[0].link;
+  result['description'] = jsonObject.rss.channel[0].description;
+  result['currentTrack'] = {
+    'title' : '',
+    'url': '',
+    'offset': ''};
+
+  jsonObject.rss.channel[0].item.forEach((item) => {
+    chapters.push({
+      'title': item.title[0],
+      'url': item.link[0]
+    });
+  });
+
+  result['chapters'] = chapters;
+  // console.log(result);
+  return result;
+}
+
+// httpGet('https://librivox.org/rss/253').then((data) => {
+//   buildBookData(data);
 // });
 
-// getUrls('https://librivox.org/rss/253', (data) => {
-//   console.log(data);
-// });
+// httpGet('https://librivox.org/api/feed/audiobooks?title=^charles&format=json')
+//   .then((data) => {
+//     buildSearchResults(data);
+//   });
 
-// getFirstBook(options, (data) => {
-//   console.log(data);
-// })
+
 
 exports.handler = function (event, context) {
-    const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  const alexa = Alexa.handler(event, context);
+  alexa.APP_ID = APP_ID;
+  alexa.dynamoDBTableName = 'mina';
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
