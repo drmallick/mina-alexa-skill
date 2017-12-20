0c4d7a8699a97853d5fb13ea3ce469a437b276b5
Suman Saurabh <sumansaurabh44@gmail.com>
2017-12-20 14:24:33 -0500

    search and play handlers

--- lambda/index.js
+++ lambda/index.js
@@ -9,41 +9,58 @@ var xml2js = require('xml2js-es6-promise');
 var rp = require('request-promise');
 
 
+const states = {
+  CONFIRMBOOK : '_CONFIRMBOOK',
+  SEARCHBOOK: '_SEARCHBOOK'
+};
+
 const handlers = {
-    'LaunchRequest': function () {
-        this.emit(':ask', "What book would you like to listen to ?");
+    'NewSession': function () {
+      this.handler.state = states.SEARCHBOOK;
+      this.emitWithState('NewSession');
     },
 
-    'SearchBook': function(event, handler) {
+  'LaunchRequest': function () {
+    this.handler.state = '';
+    delete this.attributes['STATE'];
+    this.response.listen('What book would you like to listen to ?');
+    this.emit(':responseReady');
+  }
+};
 
-      var slots = this.event.request.intent.slots;
+const searchHandlers = Alexa.CreateStateHandler(states.SEARCHBOOK, {
 
-      if(slots.bookName.value) {
-
-        var options = {
-          parameter: 'title',
-          feedname: 'audiobooks',
-          query: slots.bookName.value,
-          format: 'json'
-        };
-
-        var output = "Finding audiobooks for the book " + slots.bookName.value;
-
-        var url = buildUrl(options);
-        httpGet(url).then((data) => {
-          var results = buildSearchResults(data);
-          this.attributes['searchResults'] = results;
-          this.attributes['confirmIndex'] = 0;
-          this.handler.state = states.CONFIRMBOOK;
-          this.response.listen('Did you mean ' + results[0].title + ' by ' +
-                              results[0].authors.join(', '));
-          this.emit(':responseReady');
-        });
-      } else {
-        this.emit(':tell', "Charles Dickens");
-      }
-
+    'NewSession': function(event, handler) {
+      this.emit(':ask', "What would you like to do ?");
     },
+
+  'SearchBook': function(event, handler) {
+    var slots = this.event.request.intent.slots;
+
+    if(slots.bookName.value) {
+
+      var options = {
+        parameter: 'title',
+        feedname: 'audiobooks',
+        query: slots.bookName.value,
+        format: 'json'
+      };
+
+      var output = "Finding audiobooks for the book " + slots.bookName.value;
+      // this.emit(':ask', output);
+
+      var url = buildUrl(options);
+      httpGet(url).then((data) => {
+        var results = buildSearchResults(data);
+        this.attributes['searchResults'] = results;
+        this.attributes['confirmIndex'] = 0;
+        this.handler.state = states.CONFIRMBOOK;
+        this.emit(':ask', 'Did you mean ' + results[0].title + ' by ' +
+                            results[0].authors.join(', '));
+      });
+    } else if(slots.authorName.value) {
+    }
+  },
 
     'AMAZON.HelpIntent': function () {
         const speechOutput = this.t('HELP_MESSAGE');
@@ -56,15 +73,52 @@ const handlers = {
     'AMAZON.StopIntent': function () {
         this.emit(':tell', this.t('STOP_MESSAGE'));
     },
-};
+
+  'Unhandled': function() { 
+    // console.log(this.event);
+    // console.log(this.event.request.intent.slots);
+    this.response.speak('Sorry, I didn\'t get that.').listen('Try again');
+    this.emit(':responseReady');
+  }
+});
 
 
-var options = {
-  feedname: "audiobooks",
-  parameter: "title",
-  query: "pride and prejudice",
-  format: "json"
-}
+const playHandlers = Alexa.CreateStateHandler(states.CONFIRMBOOK, {
+  'NewSession': function() {
+    this.emit('NewSession');
+  },
+
+  'AMAZON.YesIntent': function() {
+    var index = this.attributes['confirmIndex'];
+    var book = this.attributes['searchResults'][index];
+    httpGet(book.url).then((data) => {
+      var bookData = buildBookData(data);
+      this.attributes['currentBook'] = bookData;
+      var firstTitle = bookData.chapters[0].title;
+      var firstURL= bookData.chapters[0].url.replace(/http/, 'https');
+      console.log(firstTitle);
+      this.response.speak('Playing ' + firstTitle);
+      this.response.audioPlayerPlay('REPLACE_ALL', firstURL, firstTitle, null, 0);
+      this.emit(':responseReady');
+    }).catch((error) => {
+      console.log(error);
+    });
+
+  },
+  'Unhandled': function() { 
+    // console.log(this.event);
+    // console.log(this.event.request.intent.slots);
+    this.response.speak('Sorry, I didn\'t get that.').listen('Try again');
+    this.emit(':responseReady');
+  }
+
+});
+// var options = {
+//   feedname: "audiobooks",
+//   parameter: "title",
+//   query: "pride and prejudice",
+//   format: "json"
+// }
 
 var xmlTransform = (body, response, resolveWithFullResponse) => {
   return xml2js(body).then((result) => {
@@ -85,13 +139,9 @@ function httpGet(url) {
     options['json'] = true;
   }
 
-  return rp(options)
-    .then(data => {
-      return data;
-    })
-    .catch(error => {
-      console.log(error);
-    });
+
+  console.log(url);
+  return rp(options);
 }
 
 function buildSearchResults(jsonObject) {
@@ -118,35 +168,12 @@ function buildUrl(options) {
         encodeURIComponent(options.query) +
         '&format=' + options.format;
   var url = baseUrl + reqUrl;
+  console.log("here1 " + url);
   return url;
 
 }
 
-const states = {
-  CONFIRMBOOK : '_CONFIRMBOOK'
-};
 
-const playHandlers = Alexa.CreateStateHandler(states.CONFIRMBOOK, {
-  'NewSession': function() {
-    this.emit('NewSession');
-  },
-
-  'AMAZON.YesIntent': function() {
-    var index = this.attributes['confirmIndex'];
-    var book = this.attributes['searchResults'][index];
-    httpGet(book.url).then((data) => {
-      var bookData = buildBookData(data);
-      this.attributes['currentBook'] = bookData;
-      var firstTitle = bookData.chapters[0].title;
-      var firstURL= bookData.chapters[0].url.replace(/http/, 'https');
-      console.log(firstTitle);
-      this.response.speak('Playing ' + firstTitle);
-      this.response.audioPlayerPlay('REPLACE_ALL', firstURL, firstTitle, null, 0);
-      this.emit(':responseReady');
-      });
-
-  }
-});
 function buildBookData(jsonObject) {
   var result = {};
   var chapters = [];
@@ -171,7 +198,7 @@ function buildBookData(jsonObject) {
 }
 
 // httpGet('https://librivox.org/rss/253').then((data) => {
-//   buildBookData(data);
+//   console.log(data);
 // });
 
 // httpGet('https://librivox.org/api/feed/audiobooks?title=^charles&format=json')
@@ -186,6 +213,7 @@ exports.handler = function (event, context) {
   alexa.APP_ID = APP_ID;
   alexa.dynamoDBTableName = 'mina';
   alexa.registerHandlers(handlers,
+                         searchHandlers,
                         playHandlers);
   alexa.execute();
 };
